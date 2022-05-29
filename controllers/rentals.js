3fcceca43c2a55866142b739eb5a941dd8d78d36
Lucas klafke <lucasnkz11@gmail.com>
2022-05-29 20:51:29 -0300

    fix: validateRent

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -53,16 +53,17 @@ export async function getRents(req, res) {
 export async function postRent(req, res) {
   const { customerId, gameId, daysRented } = req.body;
   const rentDate = dayjs().format("YYYY-MM-DD");
-  const { game } = res.locals;
-  console.log(game);
-  const originalPrice = game.pricePerDay * daysRented;
+  const { pricePerDay } = res.locals;
+  console.log(pricePerDay)
+  const originalPrice = pricePerDay * daysRented;
 
   try {
     const postResult = await connection.query(
       `INSERT INTO rentals ("customerId","gameId","daysRented","rentDate","originalPrice","delayFee","returnDate")
-                  values($1,$2,$3,$4,$5,null,null)`,
+        VALUES($1,$2,$3,$4,$5,null,null)`,
       [customerId, gameId, daysRented, rentDate, originalPrice]
     );
+
     res.sendStatus(201);
   } catch (err) {
     res.send(err);
--- middlewares/validateRent.js
+++ middlewares/validateRent.js
@@ -1,9 +1,12 @@
 import connection from "../db.js";
 import validateRentSchema from "../schemas/validateRentSchema.js";
 export default async function validateRent(req, res, next) {
+
+
         const validation = validateRentSchema.validate(req.body);
+
         if(validation.error){
-                return res.sendStatus(validation.error.message)
+            return res.status(400).send(validation.error.message)
         }
         const {customerId,gameId} = req.body
         try{
@@ -13,31 +16,34 @@ export default async function validateRent(req, res, next) {
                 if((customerResult.rows).length <= 0){
                         return res.status(400).send("customer not found!")
                 }
+
+
                 const gameResult = await connection.query(
                         `SELECT * from games where id = $1`,[gameId]
                 )
-                if((gameResult).length <= 0 ){
+                if((gameResult.rows).length <= 0 ){
                         return res.status(400).send("game not found!")
                 }
-                const rentResult = await connection.query(
-                        `SELECT * from rentals where "gameId" = $1`,[gameId]
-                )
-                let rentAvaible = true
-                const rentList = rentResult.rows
 
-                rentList.forEach((rent) => {
-                  if (rent.returnDate === null) {
-                          rentAvaible = false
-                  }
-                });
-                if(rentAvaible){
-                        const game = (gameResult.rows[0])
-                        res.locals.game = game
-                        next()
-                } else{
-                    return res.status(400).send("rent not avaible!");                
-                }
+        
+                const rentResult = await connection.query(
+                  `SELECT rentals."returnDate", games."stockTotal", games."pricePerDay" from rentals
+                        join games
+                                on rentals."gameId"= games.id
+                        where rentals."returnDate" is null and games.id = $1`,
+                  [gameId]
+                );
+
+                const rowsLength = (rentResult.rows).length
+                const stockTotal = rentResult.rows[0].stockTotal
                 
+                if(rowsLength > stockTotal){
+                        return res.sendStatus(400)
+                }
+                console.log(rentResult.rows)
+                const pricePerDay = rentResult.rows[0].pricePerDay;
+                res.locals.pricePerDay = pricePerDay
+                next()
 
         }catch(err){
                 return res.send(err)
